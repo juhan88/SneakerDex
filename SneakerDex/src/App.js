b753b29abec35b63ba856d669f82cb3f8ff6a8af
tamabo88 <tamabo88@yahoo.ca>
2016-12-27 11:26:53 -0800

    Use material-ui to test buttons and nav

--- SneakerDex/src/App.js
+++ SneakerDex/src/App.js
@@ -1,17 +1,59 @@
-import Navigation from 'react-toolbox/lib/navigation';
-import Link from 'react-toolbox/lib/link';
+import React from 'react';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import Tile from './Tiles/Tile'
+import Toggle from 'material-ui/Toggle';
+const SneakerDexHeader = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <AppBar title="SneakerDex" />
 
-const actions = [
-  { label: 'Alarm', raised: true, icon: 'access_alarm'},
-  { label: 'Location', raised: true, accent: true, icon: 'room'}
-];
-
-const NavigationTest = () => (
-  <div>
-    <Navigation type='horizontal' actions={actions} />
-    <Navigation type='vertical'>
-      <Link href='http://' label='Inbox' icon='inbox' />
-      <Link href='http://' active label='Profile' icon='person' />
-    </Navigation>
-  </div>
+  </MuiThemeProvider>
 );
+
+const styles = {
+  block: {
+    maxWidth: 250,
+  },
+  toggle: {
+    marginBottom: 16,
+  },
+  thumbOff: {
+    backgroundColor: '#ffcccc',
+  },
+  trackOff: {
+    backgroundColor: '#ff9d9d',
+  },
+  thumbSwitched: {
+    backgroundColor: 'red',
+  },
+  trackSwitched: {
+    backgroundColor: '#ff9d9d',
+  },
+  labelStyle: {
+    color: 'red',
+  },
+};
+
+
+
+
+class App extends React.Component {
+  render() {
+    return <div className="App">
+        <SneakerDexHeader/>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <div style={styles.block}>
+          <Toggle
+           label="Simple"
+           style={styles.toggle}
+          />
+          </div>
+        </MuiThemeProvider>
+        <Tile name={this.props.name}/>
+      </div>
+  }
+}
+
+export default App
